90473768696cd81bbcd268bc1d4d59d43ed8b0fe
Gireesh Patgar <gireeshpatgar17@gmail.com>
2025-09-18 10:46:42 +0530

    None

--- src/main/java/com/mid/ecom_server/repos/UserRepo.java
+++ src/main/java/com/mid/ecom_server/repos/UserRepo.java
@@ -1,6 +1,5 @@
 package com.mid.ecom_server.repos;
 import java.util.*;
-import org.*;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mid.ecom_server.models.User;
