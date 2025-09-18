d44e81718775dbd4e92df047253dec1e9ad86767
Amogh S Acharya <2023ci_amoghsacharya_a@nie.ac.in>
2025-09-18 10:28:06 +0530

    commit

--- src/main/java/com/mid/ecom_server/repos/OrderRepo.java
+++ src/main/java/com/mid/ecom_server/repos/OrderRepo.java
@@ -1,3 +1,8 @@
+
+import java.util.*;
+import org.*;
+
+
 public interface OrderRepo extends MongoRepository<Order, String>{ 
     List<Order> findUserById(String userId);
 }
--- src/main/java/com/mid/ecom_server/repos/UserRepo.java
+++ src/main/java/com/mid/ecom_server/repos/UserRepo.java
@@ -1,3 +1,8 @@
+// package com.mid.ecom_server.repos;
+import java.util.*;
+import org.*;
+
+
 public interface UserRepo extends MongoRepository <User, String>{
     Optional<User> findByEmail(String email);
 }
