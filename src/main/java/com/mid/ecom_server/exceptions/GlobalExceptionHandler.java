8ae93f9f620dce5d1efe8d6409b7d4f0983d1a0b
Darshank2424 <darshank2424@gmail.com>
2025-09-18 11:43:43 +0530

    Global Exception handler commented code added

--- src/main/java/com/mid/ecom_server/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/mid/ecom_server/exceptions/GlobalExceptionHandler.java
@@ -2,7 +2,16 @@ package com.mid.ecom_server.exceptions;
 
 
 
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//@ControllerAdvice
 public class GlobalExceptionHandler {
-
-
+	
+//	@ExceptionHandler(ResourceNotFoundException.class)
+//	public ResponseEntity<String> handleNotFound(String msg){
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+//	}
 }
