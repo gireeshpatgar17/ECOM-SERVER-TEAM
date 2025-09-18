2b30b47bd5277802bf9326df1a84f17e29b515f5
Gireesh Patgar <gireeshpatgar17@gmail.com>
2025-09-18 10:32:01 +0530

    Errors corrected

--- src/main/java/com/mid/ecom_server/repos/OrderRepo.java
+++ src/main/java/com/mid/ecom_server/repos/OrderRepo.java
@@ -1,6 +1,9 @@
-// package com.mid.ecom_server.repos;
+package com.mid.ecom_server.repos;
 import java.util.*;
 import org.*;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.mid.ecom_server.models.Order;
 
 
 public interface OrderRepo extends MongoRepository<Order, String>{ 
